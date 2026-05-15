63dab0a8a544b1b9d8cf4459b1f7ec856df0fff7
Samira <samira@mail.com>
2026-05-15 11:24:50 +0300

    remove meme.py, change datail.html | comment.html | create.html
    This reverts commit df12cf7ef8cb8e34411baabe713b147dc3f7beba.

--- blogicum/blog/views/__init__.py
+++ blogicum/blog/views/__init__.py
@@ -1,29 +1,17 @@
-# blog/views/__init__.py
-from .posts import (
-    index,
-    post_detail,
-    category_posts,
-    create_post,
-    edit_post,
-    delete_post,
-)
+from .posts import index, post_detail, category_posts, create_post, edit_post, delete_post
 from .comments import add_comment, edit_comment, delete_comment
 from .profile import profile, edit_profile
-from .meme import generate_meme, meme_gallery, save_meme_as_post
 
 __all__ = [
-    "index",
-    "post_detail",
-    "category_posts",
-    "create_post",
-    "edit_post",
-    "delete_post",
-    "add_comment",
-    "edit_comment",
-    "delete_comment",
-    "profile",
-    "edit_profile",
-    "generate_meme",
-    "meme_gallery",
-    "save_meme_as_post",
+    'index',
+    'post_detail',
+    'category_posts',
+    'create_post',
+    'edit_post',
+    'delete_post',
+    'add_comment',
+    'edit_comment',
+    'delete_comment',
+    'profile',
+    'edit_profile',
 ]
